23a87105ddca66c2843c359650eb51acbd0c4b3e
waqas-engineer <waqasengineer9733@gmail.com>
2024-04-24 10:03:38 +0500

    UseEffect, UseRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [lenght, setLenght] = useState(8);
+  const [length, setLenght] = useState(8);
   const [number, setNumber] = useState(false);
   const [character, setCharacter] = useState(false);
   const [password, setPassword] = useState("");
+
+  //UseRef
+  const passwordRef = useRef(null);
 
   const PasswordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (number) str += "0123456789";
     if (character) str += "!@#$%^&*()-_=+|[]{};:/?";
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
       setPassword(pass);
     }
-  }, [lenght, number, character, setPassword]);
+  }, [length, number, character, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    PasswordGenerator();
+  }, [length, number, character, PasswordGenerator]);
+
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
-        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <h1 className="text-white text-lg text-center my-3">
+          Password Generator
+        </h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
@@ -28,8 +44,12 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 ">
+          <button
+            onClick={copyPasswordToClipboard}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 "
+          >
             copy
           </button>
         </div>
@@ -39,13 +59,13 @@ function App() {
               type="range"
               min={8}
               max={100}
-              value={lenght}
+              value={length}
               className="cursor-pointer"
               onChange={(e) => {
                 setLenght(e.target.value);
               }}
             />
-            <label>Length: {lenght}</label>
+            <label>Length: {length}</label>
           </div>
           <div className="flex items-center gap-x-1">
             <input
